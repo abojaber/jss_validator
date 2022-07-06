da751e40f72d37964e24f45074736fbe13d80311
Fahad Al-Hantushi <abojaber@programmer.net>
2022-07-06 12:35:56 +0300

    write 13 test cases

--- test/test.js
+++ test/test.js
@@ -2,8 +2,6 @@ var roles = require("../roles.js");
 var payload = require("../payload.js");
 var validate = require("../validator.js");
 
-console.log(payload_text);
-
 var assert = require("assert");
 
 describe("Regex validation: ", function () {
@@ -35,5 +33,28 @@ describe("Regex validation: ", function () {
                 "ST0001"
             );
         });
+        it("Generate error if not a saudi mobile number");
     });
+});
+describe("Numbers", function () {
+    it("Generate error if field not equal expected");
+    it("Generate error if field less than fixed number");
+    it("Generate error if field more than expected");
+});
+
+describe("Dates", function () {
+    it("Generate error if field is before expected");
+    it("Generate error if field is after expected");
+});
+
+describe("Customized by function", function () {
+    it("Generate error based on function return ");
+});
+describe("Required", function () {
+    it("Generate error if field equal value");
+    it("Generate error if field greater than value");
+    it("Generate error if field greater than or equal value");
+    it("Generate error if field lest than value");
+    it("Generate error if field lest than or equal value");
+    it("Generate error if field in a defined list");
 });
